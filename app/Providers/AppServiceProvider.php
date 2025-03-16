d4732de5558fd44f9eefcf6b75e319cb643d762b
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-03-16 12:08:27 -0500

    refactor: remove unused model observers from AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,17 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\Product;
-use App\Models\Category;
-use App\Models\Place;
-use App\Models\ProductStatus;
-use App\Models\Unit;
-use App\Models\PurchasesHistoryHeader;
-use App\Models\PurchasesHistoryDetail;
-use App\Models\ChecklistHeader;
-use App\Models\ChecklistDetail;
-use App\Models\NeededProduct;
-use App\Observers\GenericObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,14 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Registrar los Observers
-        Product::observe(GenericObserver::class);
-        Category::observe(GenericObserver::class);
-        Place::observe(GenericObserver::class);
-        ProductStatus::observe(GenericObserver::class);
-        Unit::observe(GenericObserver::class);
-        PurchasesHistoryHeader::observe(GenericObserver::class);
-        PurchasesHistoryDetail::observe(GenericObserver::class);
-        ChecklistHeader::observe(GenericObserver::class);
-        ChecklistDetail::observe(GenericObserver::class);
     }
 }
